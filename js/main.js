abd92785c530f335c9797098de76c52dae5b6559
Isiaka Lukman Bamidele <isiakalukmandellyson@gmail.com>
2019-10-10 00:26:05 +0100

    setup ready

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-AOS.init();
-$(".slicky").slick({
-  adaptiveHeight: true
-});
+// AOS.init();
+// $(".slicky").slick({
+//   adaptiveHeight: true
+// });
 
-var slider = tns({
-  container: ".my-slider",
-  items: 1,
-  autoplay: true,
-  autoplayButtonOutput: false,
-  controls: false
+// var slider = tns({
+//   container: ".my-slider",
+//   items: 1,
+//   autoplay: true,
+//   autoplayButtonOutput: false,
+//   controls: false
+// });
+
+document.querySelector("#open").addEventListener("click", function(e) {
+  e.preventDefault();
+  document.querySelector(".side-nav").style.width = "100%";
+  document.getElementById("main").style.marginRight = "550px";
+});
+document.querySelector("#close").addEventListener("click", function(e) {
+  e.preventDefault();
+  document.querySelector(".side-nav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
 });
